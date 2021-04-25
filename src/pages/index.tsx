9a9714fa7029610df2fe389e3e583d3007107819
Leonardo <leomarques301@gmail.com>
2021-04-25 12:37:35 -0300

    Header and homepage are now responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,56 +68,42 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
-            );
+            ) 
           })}
         </ul>
       </section>
 
       <section className={styles.allEpisodes}>
           <h2>Todos episódios</h2>
+          <ul>
+            {allEpisodes.map((episode, index) => {
+              return (
+                <li key={episode.id}>
+                  <Image 
+                    width={120} 
+                    height={120} 
+                    src={episode.thumbnail} 
+                    alt={episode.title}
+                    objectFit="cover"
+                  />
 
-          <table cellSpacing={0}>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Podcast</th>
-                <th>Integrantes</th>
-                <th>Data</th>
-                <th>Duração</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {allEpisodes.map((episode, index) => {
-                return (
-                  <tr key={episode.id}>
-                    <td style={{width: 90}}>
-                      <Image 
-                        width={120}
-                        height={120}
-                        src={episode.thumbnail}
-                        alt={episode.title}
-                        objectFit="cover"
-                      />
-                    </td>
-                    <td>
-                      <Link href={`/episodes/${episode.id}`}>
-                        <a>{episode.title}</a>
-                      </Link>
-                    </td>
-                    <td>{episode.members}</td>
-                    <td style={{ width: 100 }}>{episode.publishedAt}</td>
-                    <td>{episode.durationAsString}</td>
-                    <td>
-                      <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
-                        <img src="/play-green.svg" alt="Tocar episódio"/>
-                      </button>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
+                  <div className={styles.episodeDetails}>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                    <p>{episode.members}</p>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationAsString}</span>
+                  </div>
+                  
+                  <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
+                    <img src="/play-green.svg" alt="Tocar episódio"/>
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
+    
       </section>
     </div>
   )
